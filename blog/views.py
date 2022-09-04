40ca2b1aec1c4de4434a9e257c9f6d8e156262d5
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2022-09-04 22:03:54 +0530

    Fixes

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,7 @@ import datetime
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
-    context = {
-        "posts" : posts,
-    }
-    return render(request, "blog/post_list.html", context)
+    return render(request, "blog/post_list.html", {'posts' : posts})
 
 
 def post_detail(request, pk):
