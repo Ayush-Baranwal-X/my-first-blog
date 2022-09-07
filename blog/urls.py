cd98032e063d23a4d5451ae2cac5e1ae100da9cd
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2022-09-07 18:30:34 +0530

    Fixes

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.postList, name='post_list'),
-    path('post/<int:pk>/', views.postDetail, name='post_detail'),
     path('post/new', views.postNew, name='post_new'),
+    path('post/<int:pk>/', views.postDetail, name='post_detail'),
     path('post/edit/<int:pk>', views.postEdit, name='post_edit')
 ]
