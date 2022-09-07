2a8eabd389614f213ef6c1e70ae79bf719c1a86a
Ayush Kumar Baranwal <ayushabhilasha@gmail.com>
2022-09-07 17:57:11 +0530

    Fixes

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.postList, name='post_list'),
-    path('post/<int:pk>/', views.postDetail, name='post_detail'),
-    path('post/new',views.postNew, name = 'post_new'),
-    path('post/edit/<int:pk>',views.postEdit, name = 'post_edit')
+    path('', views.postList, name="post_list"),
+    path('post/<int:pk>/', views.postDetail, name="post_detail"),
+    path('post/new', views.postNew, name="post_new"),
+    path('post/edit/<int:pk>', views.postEdit, name="post_edit")
 ]
